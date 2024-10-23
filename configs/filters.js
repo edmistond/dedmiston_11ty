ba79c3cfccc1548f35f4d047398b6874d1424fa1
David Edmiston <edmistond@gmail.com>
2024-10-22 21:25:29 -0400

    additional tweaks

--- configs/filters.js
+++ configs/filters.js
@@ -1,9 +1,13 @@
 import { DateTime } from "luxon";
 
 function filterConfig(eleventyConfig) {
-    eleventyConfig.addFilter("readableDate", (date) => {
-        return DateTime.fromJSDate(date, { zone: "utc" }).toFormat("dd LLL yyyy");
-      });    
+  eleventyConfig.addFilter("readableDate", (date) => {
+    return DateTime.fromJSDate(date, { zone: "utc" }).toFormat("dd LLL yyyy");
+  });
+
+  eleventyConfig.addShortcode("buildYear", () => {
+    return `${new Date().getFullYear()}`;
+  });
 }
 
 export { filterConfig };
