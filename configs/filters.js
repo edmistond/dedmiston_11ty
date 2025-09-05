ab527fcfc36ee8a25866a0681965a6c6831050c3
David Edmiston <edmistond@gmail.com>
2025-09-05 15:30:18 -0400

    [codex] add machineDate filter for semantic <time> elements

--- configs/filters.js
+++ configs/filters.js
@@ -5,6 +5,10 @@ function filterConfig(eleventyConfig) {
     return DateTime.fromJSDate(date, { zone: "utc" }).toFormat("dd LLL yyyy");
   });
 
+  eleventyConfig.addFilter("machineDate", (date) => {
+    return DateTime.fromJSDate(date, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
   eleventyConfig.addShortcode("buildYear", () => {
     return `${new Date().getFullYear()}`;
   });
