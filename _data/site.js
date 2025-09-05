58c5019037bd0c8f0150d0fe10e85033a4b61022
David Edmiston <edmistond@gmail.com>
2025-09-05 15:29:01 -0400

    [codex] add site title and description for head metadata

--- _data/site.js
+++ _data/site.js
@@ -1,5 +1,7 @@
 export default {
     url: process.env.INPUT_SITE_URL || "https://davidedmiston.com",
+    title: "David Edmiston",
+    description: "Developer, writer, and accessibility advocate. Posts on engineering, tooling, and captions.",
     author: "David Edmiston",
-    time: new Date(),    
+    time: new Date(),
 };
