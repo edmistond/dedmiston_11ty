e0b3817e13882c7a5c415a475a694404d8d17541
David Edmiston <edmistond@gmail.com>
2025-11-01 19:40:22 -0400

    Switch to vibe-coded footnotes library

--- configs/markdown.js
+++ configs/markdown.js
@@ -1,15 +1,15 @@
 import markdownIt from "markdown-it";
-import markdownItFootnote from "markdown-it-footnote";
+import edmistondFootnote from "edmistond-footnotes";
 
 function markdownConfig(eleventyConfig) {
-    const markdownOptions = {
-        html: true,
-        breaks: true,
-        linkify: true,
-      };
-    
-      let markdownLibrary = markdownIt(markdownOptions).use(markdownItFootnote);
-      eleventyConfig.setLibrary("md", markdownLibrary);    
+  const markdownOptions = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };
+
+  let markdownLibrary = markdownIt(markdownOptions).use(edmistondFootnote);
+  eleventyConfig.setLibrary("md", markdownLibrary);
 }
 
 export { markdownConfig };
